6d4bbc5d46e2898599deb098cf386370698e03ab
gyan9427 <gyan9427@gmail.com>
2022-02-03 21:48:37 +0530

    create notes to understand the diffence in interface application in JS

--- CitiBank.js
+++ CitiBank.js
@@ -11,3 +11,6 @@ class CitiBank extends Bank{
 
 module.exports = Bank;
 
+//Pay and refund are made as interface methods
+
+// Since in case of JS there is no actual implementation of interface so here an interface instead of making it as the most dynamic one, over here the implementation is made very much general one
